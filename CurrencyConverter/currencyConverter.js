c62d5e00b72809664bbdcaf1c82d8748d7dc6dec
CodeBard_x88 <solo.developer29@gmail.com>
2024-05-01 00:32:58 +0500

    Updated API Project

--- CurrencyConverter/currencyConverter.js
+++ CurrencyConverter/currencyConverter.js
@@ -1,7 +1,25 @@
 
-const URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur/jpy.json";
+const BASE_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/";
 
 let dropdowns = document.querySelectorAll(".dropdowns select");
+let btn = document.querySelector(".btn button");
+
+btn.addEventListener("click", () => {
+    let amount = document.querySelector(".enter input");
+    let amountval = amount.value;
+
+    let convert_from = document.querySelector(".from select");
+    let from_country = convert_from.value;
+
+    let convert_to =  document.querySelector(".to select");
+    let to_country = convert_to.value;
+
+    const URL =   `${BASE_URL}/${from_country.toLowerCase()}/${to_country.toLowerCase()}.json`
+
+    fetch(URL).then((res)=>{
+        console.log(res);
+    })
+})
 
 for (let select of dropdowns){
     for(let currcode in countryList){
@@ -17,5 +35,5 @@ for (let select of dropdowns){
 }
 
 const updateFlag = (el) => {
-    let img = el.parentElement.querySelector("img").src = `https://flagsapi.com/${countryList[el.value]}/shiny/64.png`
+    el.parentElement.querySelector("img").src = `https://flagsapi.com/${countryList[el.value]}/shiny/64.png`
 }
